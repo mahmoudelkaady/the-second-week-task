eef21e0288b2ec19c443602bdc37d0d1eadf0e94
mahmoud medhat <mahmodmht.8.8@gmail.com>
2023-04-18 09:40:50 +0200

    the second week done

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let numberOne = "10";
+let numberTwo = "20";
+let n = "\n";
+console.log(numberOne + numberTwo);
+console.log(typeof 1020);
+console.log(`${numberOne}${numberTwo}`);
+console.log(numberOne + "\n" + numberTwo);
+console.log(numberOne + n + numberTwo);
+console.log(`${numberOne}
+${numberTwo}
+`);
+console.log(`${numberOne}${n}${numberTwo}`);
+
+//
+
+// console.log(elzero.innerHTML); // object
+// console.log(typeof elzero); // object
+
+//
+
+let a = 21;
+let b = 20;
+
+console.log("_" + a + "_" + b + a + "_" + b + a + "_" + b + a + "_" + b + "_"); // _21_2021_2021_2021_20_
